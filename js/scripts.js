2d02a8de9dc1d51615b6be2d65218deb8a5aae46
Bryan Lin and Jesse Hoare Evans <bryan@>
2017-03-20 10:47:44 -0700

    add html file, javascript file and readme

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2
+};
+
+var number1= parseFloat(prompt("add a number"));
+var number2= parseFloat(prompt("add another number"));
+
+alert (add(number1, number2));
